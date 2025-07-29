b0e32841e4e38e0b9b9160ce0ebff0f38f0d429e
mikic202 <mikolaj.chomanski@gmail.com>
2025-07-29 21:29:12 +0200

    Add pdf ingestion

--- rag_microservice/gateway.py
+++ rag_microservice/gateway.py
@@ -1,26 +1,31 @@
-from flask import Flask, Response
-from rag_microservice.rag.rag import RAG
-from rag_microservice.embeder.local_embedings_manager import LocalEmbeddingsManager
-from rag_microservice.embeder.embeder import Embedder
+from flask import Flask, Response, request, redirect, flash
+from rag.rag import RAG
+from embeder.local_embedings_manager import LocalEmbeddingsManager
+from embeder.embeder import Embedder
+from werkzeug.utils import secure_filename
+from pathlib import Path
 import vertexai
 
 
 class Gateway(Flask):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.add_url_rule("basic", "basic", self.get_basic_llm_answer, methods=["GET"])
         self.add_url_rule(
-            "rag",
-            "rag",
+            "/basic", "/basic", self.get_basic_llm_answer, methods=["GET"]
+        )
+        self.add_url_rule(
+            "/rag",
+            "/rag",
             self.get_rag_answer,
             methods=["GET"],
         )
         self.add_url_rule(
-            "add_user_file",
-            "add_user_file",
+            "/add_user_file/<user_id>",
+            "/add_user_file/<user_id>",
             self.add_user_file,
             methods=["POST"],
         )
+        self.__file_directory = Path("user_files")
         vertexai.init(project="basic-rag-456110", location="europe-west1")
         self.__embedder = Embedder("text-embedding-005", 512)
         self.__embedding_manager = LocalEmbeddingsManager(
@@ -50,5 +55,16 @@ class Gateway(Flask):
             mimetype="text/plain",
         )
 
-    def add_user_file(self, user_id: int, file: str) -> str:
-        return f"File {file} added for user {user_id}"
+    def add_user_file(self, user_id) -> str:
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+        if not file:
+            return Response("No file selected", status=400, mimetype="text/plain")
+        filename = secure_filename(file.filename)
+        file_directory = self.__file_directory / str(user_id)
+        file_directory.mkdir(parents=True, exist_ok=True)
+        file.save(file_directory / filename)
+        self.__rag.ingest_pdf(str(file_directory / filename), user_id)
+        return Response("Successfully uploaded file", status=200, mimetype="text/plain")
